468675b5d4ca7fef0695638449e3eb69bd96c7e6
devanaclimgo <anaclimgo@gmail.com>
2025-02-08 13:02:18 -0300

    palindrome checker - reverse string function js

--- Palindrome-Checker/script.js
+++ Palindrome-Checker/script.js
@@ -1,6 +1,10 @@
 const input = document.getElementById("input")
 
+function reverseString(str) {
+  return str.split("").reverse().join("")
+}
+
 function check() {
   const value = input.value
-  alert(value)
+
 }
