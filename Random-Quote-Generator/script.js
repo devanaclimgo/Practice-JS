6f7378894d762c78b5f61d5cb0bdf9615f75ed59
devanaclimgo <anaclimgo@gmail.com>
2025-02-08 21:15:14 -0300

    rqg - quotes added js

---
+++ Random-Quote-Generator/script.js
@@ -0,0 +1,13 @@
+const quotes = [
+  "The purpose of our lives is to be happy.",
+  "Get busy living or get busy dying.",
+  "You only live once, but if you do it right, once is enough.",
+  "Many of life's failures are people who did not realize how close they were to success when they gave up.",
+  "If you want to live a happy life, tie it to a goal, not to people or things.",
+  "Never let the fear of striking out keep you from playing the game.",
+  "Money and success don't change people; they merely amplify what is already there.",
+  "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma - which is living with the results of other people's thinking.",
+  "Not how long, but how well you have lived is the main thing.",
+  "If life were predictable it would cease to be life, and be without flavor.",
+  "The whole secret of a successful life is to find out what is one's destiny to do, and then do it."
+]
